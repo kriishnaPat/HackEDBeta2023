61c179d5c39a21ffabce0ea6d47c5f2db15512bf
Divya <d.prasad.8018@gmail.com>
2023-11-18 16:47:18 -0700

    message

--- app.py
+++ app.py
@@ -58,6 +58,20 @@ def profile():
         return redirect('/login')
     else:
         return redirect('/')
+    
+@app.route("/v1/message/schedule", methods=["POST"])
+def send_scheduled_texts():
+    try:
+        data = request.get_json()
+        to_number = data.get("number")
+        minutes_ahead = data.get("minutes")
+        message = data.get("message")
+
+        schedule_message(to_number, minutes_ahead, message)
+        return Response('{"status": "Message sent successfully"}', status=201, mimetype='application/json')
+    except Exception as e:
+        print(e)
+        return Response('{"status": "Message was not sent"}', status=500, mimetype='application/json')
 
 if __name__ == '__main__':
 	app.run()
