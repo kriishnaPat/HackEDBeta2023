e286a05175605d6463a79eac645efa6a54f44cce
Divya <d.prasad.8018@gmail.com>
2023-11-18 12:42:41 -0700

    hiding api key

--- send_sms.py
+++ send_sms.py
@@ -1,13 +1,12 @@
+import os
 from twilio.rest import Client
 
-account_sid = 'ACa20945303efc874af274f6e9705a84a7'
-auth_token = '2359e38d1b0d6a80e4ad9468c5cc82cf'
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-  from_='+14149821518',
-  body='how much wood would a wood chuck chuck if a wood chuck if a wood chuck chuck?',
-  to='+17804059975'
+    from_="+14149821518", body="wood chuck chuck", to="+17804059975"
 )
 
 print(message.sid)
